9b0b0fb5c4e70bee5635b16728e59b6bd4336618
abubakr587 <faxriddinovabubakr36@gmail.com>
2025-05-24 15:20:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+ let randomSon = Math.floor(Math.random() * 100);
+
+   
+    let kopaytma = randomSon * 2;
+
+    
+    let bolinma = randomSon / 2;
+    let qoldiq = randomSon % 2;
+
+    
+    alert(
+      "1. Random son: " + randomSon + "\n" +
+      "2. 2 ga ko'paytirildi: " + kopaytma + "\n" +
+      "3. 2 ga bo'lingan: " + bolinma + "\n" +
+      "   Qoldiq: " + qoldiq
+    );
+ 
+
